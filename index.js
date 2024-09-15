fc72d29c55de7f2d9b30e924ca3030223cc563c4
unknown <creator.anirudhsingh@gmail.com>
2024-09-15 13:22:56 +0530

    Deployment

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const {
 } = require("./middlewares/authentication");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, process.env.VIEWS ?? "views"));
+app.set("views", path.join(__dirname, "./views"));
 app.use(session({
   secret: 'somethingsecret',
   resave: false,
